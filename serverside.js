555943d2f60177015239c6525fb7d7890165d218
Course Brains <99380906+Course-Brains@users.noreply.github.com>
2023-03-13 11:44:02 -0700

    viewpoint changes
    made it so that the perspective matches the turn, also stated working on the serverside

--- serverside.js
+++ serverside.js
@@ -1,12 +1,11 @@
 const http = require('http');
-const querystring = require('querystring');
 
 const server = http.createServer((request, response) => {
-	console.log(querystring.stringify(response.body))
 	response.writeHead(200, { 'Content-Type': 'text/plain' });
 	response.write('Hello, World!');
 	response.statusCode = 200
 	response.statusTest = "yay"
+	response.body = {worked:true}
 	response.end()
 });
 
