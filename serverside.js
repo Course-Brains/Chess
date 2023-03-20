ccaeca623e2cc97d73f7518fc7b02eb83af58741
Course Brains <99380906+Course-Brains@users.noreply.github.com>
2023-03-20 14:27:01 -0700

    some progress with the new validity check system
    there are a few bugs which I will add to the issues

--- serverside.js
+++ serverside.js
@@ -1,31 +1,16 @@
 const http = require('http');
+const fs = require('fs')
+const url = require('url')
 
-const server = http.createServer((request, response) => {
+var debug = true
+
+http.createServer((request, response) => {
 	response.writeHead(200, { 'Content-Type': 'text/plain' });
-	response.write('Hello, World!');
-	response.statusCode = 200
-	response.statusTest = "yay"
-	response.body = {worked:true}
-	response.end()
-});
+	response.end('hello world')
+}).listen(3000);
+console.log('server running on port 3000')
 
-server.listen(3000, () => {
-	console.log('Server running on port 3000');
-});
 // to start server use: node serverside.js when in correct directory
 /* code to fetch:
-fetch('http://localhost:3000', {
-	method: 'POST',
-	headers: {
-		'Content-Type': 'application/json'
-	},
-	body: JSON.stringify({
-		username: 'exampleUser',
-		password: 'examplePassword'
-	}),
-	mode:'no-cors'
-})
-	.then(response => {console.log(response)})
-	.then(data => console.log(data))
-	.catch(error => console.error(error));
+
 */
