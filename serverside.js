8e9259b473e41b29c2c005b53e4a81903dcdd95e
Course Brains <99380906+Course-Brains@users.noreply.github.com>
2023-04-17 09:11:54 -0700

    fixed, maybe
    there were so many bugs with the client side I don't remember them all but I think I fixed en passant, and no I haven't figured out why the serverside communication sends back an empty response but it's probably some extremely obvious thing that I had no way of knowing about

--- serverside.js
+++ serverside.js
@@ -1,16 +1,61 @@
-const http = require('http');
 const fs = require('fs')
-const url = require('url')
+const express = require('express')
+const app = express()
 
 var debug = true
+app.trace((request, response) => {//trace handling
+	if(debug){console.log('trace request made')}
+	response.writeHead(200, {'Content-Type':'application.json'})
+	response = request
+	if(response == request)
+	if(debug){console.log(response)}
+	return response.end()
+})
+app.use(express.json())//needed for conversion from json
 
-http.createServer((request, response) => {
-	response.writeHead(200, { 'Content-Type': 'text/plain' });
-	response.end('hello world')
-}).listen(3000);
-console.log('server running on port 3000')
-
+app.get('/solo', (request, response) => {//loading single player
+	fs.readFile('Chess.html', (err,data) => {
+		if(err){
+			response.writeHead(404, {'Content-Type':'text/plain'});
+			return response.end('404 file not found');
+		}
+		response.writeHead(200, {'Content-Type':'text/html'})
+		response.write(data)
+		return response.end();
+	})
+});
+app.get('/chess/:player', (request, response) => {
+	let player = request.params.player
+	if((player == 1)||(player == 2)){
+		fs.readFile('Chess.html', (err,data) =>{
+			if(err){
+				response.writeHead(404, {'Content-Type':'text/plain'})
+				return response.end('404 file not found')
+			}
+			response.writeHead(200, {'Content-Type':'text/html'})
+			response.write(data)
+			return response.end()
+		})
+	}
+	response.writeHead(404, {'Content-Type':'text/plain'})
+	return response.end('404 page not found')
+})
+app.get('/chess', (request, response) => {//page for navigation
+	fs.readFile('home.html', (err,data) => {
+		if(err){
+			response.writeHead(404, {'Content-Type':'text/plain'})
+			return response.end('404 file not found')
+		}
+		response.writeHead(200, {'Content-Type':'text/html'})
+		response.write(data)
+		return response.end()
+	})
+})
+app.use((request, response) => {//invalid link handling(redirects to home page)
+	response.writeHead(302, {'Location':'/chess'})
+	return response.end()
+})
+app.listen(3000, () => {
+	console.log('server running on port 3000')
+})
 // to start server use: node serverside.js when in correct directory
-/* code to fetch:
-
-*/
